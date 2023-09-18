12bda19c11efd8d44e2f4da62017fde8ffefd626
crolph224 <131316708+crolph224@users.noreply.github.com>
2023-09-18 18:33:45 -0400

    Update csv.php

--- lib/csv.php
+++ lib/csv.php
@@ -1,10 +1,12 @@
 <?php
-$file='products&services.csv';
-$fp =fopen($file, 'r');
-while($content=fgetcsv($fp)){
-	echo '- ', $content[0], ': ', $content[1]. '<br >';
-	echo 'Applications:'. '<br >';
-	echo '- ', $content[2]. '<br >';
-	echo '- ', $content[3]. '<br >';
+function loadPageCSV(){
+	$file='products&services.csv';
+	$fp =fopen($file, 'r');
+	while($content=fgetcsv($fp)){
+		echo '- ', $content[0], ': ', $content[1]. '<br >';
+		echo 'Applications:'. '<br >';
+		echo '- ', $content[2]. '<br >';
+		echo '- ', $content[3]. '<br >';
+	}
+	fclose($fp);
 }
-fclose($fp);
