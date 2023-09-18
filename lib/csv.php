4327f5aa87bc6758362e063874f5341137d78127
crolph224 <131316708+crolph224@users.noreply.github.com>
2023-09-18 18:32:09 -0400

    Add files via upload
    Uploading function to print products and services csv

---
+++ lib/csv.php
@@ -0,0 +1,10 @@
+<?php
+$file='products&services.csv';
+$fp =fopen($file, 'r');
+while($content=fgetcsv($fp)){
+	echo '- ', $content[0], ': ', $content[1]. '<br >';
+	echo 'Applications:'. '<br >';
+	echo '- ', $content[2]. '<br >';
+	echo '- ', $content[3]. '<br >';
+}
+fclose($fp);
