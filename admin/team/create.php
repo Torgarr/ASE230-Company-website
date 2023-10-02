2ce16c91453520b495f41e41250a9b27f6bd5696
crolph224 <131316708+crolph224@users.noreply.github.com>
2023-10-02 15:15:49 -0400

    Update create.php

--- admin/team/create.php
+++ admin/team/create.php
@@ -1,42 +1,68 @@
 <?php
-						<form method="post" name="myForm" onsubmit="return validateForm()">
-                            <p id="error-msg"></p>
-                            <div id="simple-msg"></div>
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <div class="mb-4">
-                                        <label for="name" class="text-muted form-label">Name</label>
-                                        <input name="name" id="name" type="text" class="form-control" placeholder="Enter name*">
-                                    </div>
-                                </div>
-                                <!-- end col -->
-                                <div class="col-lg-6">
-                                    <div class="mb-4">
-                                        <label for="email" class="text-muted form-label">Email</label>
-                                        <input name="email" id="email" type="email" class="form-control" placeholder="Enter email*">
-                                    </div>
-                                </div>
-                                <!-- end col -->
-                                <div class="col-md-12">
-                                    <div class="mb-4">
-                                        <label for="subject" class="text-muted form-label">Subject</label>
-                                        <input type="text" class="form-control" id="subject" name="subject" placeholder="Enter Subject.." />
-                                    </div>
+session_start();
+$file = __DIR__ . '\\..\\..\\data\\members.json';
+// Check if the form is submitted
+if(isset($_POST['submit'])){
+    // Read existing JSON data
+    $data = file_get_contents($file);
+    $data_array = json_decode($data, true);
 
-                                    <div class="mb-4 pb-2">
-                                        <label for="comments" class="text-muted form-label">Message</label>
-                                        <textarea name="comments" id="comments" rows="4" class="form-control" placeholder="Enter message..."></textarea>
-                                    </div>
+    // Extract form data
+    $new_member = array(
+        'name' => $_POST['name'],
+        'role' => $_POST['role'],
+        'bio'  => $_POST['bio']
+    );
+    if(isset($json_data['team']) && is_array($json_data['team'])){
+        // Append new member to the "team" array
+        $json_data['team'][] = $new_member;
+    } 
+    // Append new data
+    $data_array['team'][] = $new_member;
 
-                                    <button type="submit" id="submit" name="send" class="btn btn-primary">Send Message</button>
-                                </div>
-                                <!-- end col -->
-                            </div>
-                            <!-- end row -->
-                        </form>
+    // Convert array to JSON and write to the file
+    $updated_data = json_encode($data_array, JSON_PRETTY_PRINT);
+    file_put_contents($file, $updated_data);
 
+    $_SESSION['message'] = 'Member successfully added';
 
+    // Redirect to avoid form resubmission on page refresh
+    header("Location: {$_SERVER['PHP_SELF']}");
+    exit();
+}
 
-
-
+// Display the form
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Member</title>
+</head>
+<body>
+    <h2>Add New Member</h2>
+
+    <?php
+    // Display success or error message if available
+    if(isset($_SESSION['message'])){
+        echo '<p>' . $_SESSION['message'] . '</p>';
+        unset($_SESSION['message']); // Clear the message after displaying it
+    }
+    ?>
+
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <label for="inputName">Name:</label>
+        <input type="text" name="name" id="inputName" required>
+
+        <label for="inputRole">Role:</label>
+        <input type="text" name="role" id="inputRole" required>
+
+        <label for="inputBio">Bio:</label>
+        <textarea name="bio" id="inputBio" required></textarea>
+
+        <input type="submit" name="submit" value="Add Member">
+    </form>
+</body>
+</html>
