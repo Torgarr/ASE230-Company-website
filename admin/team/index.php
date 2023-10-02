44d82d5a1f332a250db09a83734eed47cbf27f49
crolph224 <131316708+crolph224@users.noreply.github.com>
2023-10-02 09:43:09 -0400

    Add files via upload

---
+++ admin/team/index.php
@@ -0,0 +1,26 @@
+<?php
+	$file = __DIR__ . '\\..\\..\\data\\members.json';
+    $content = file_get_contents($file);
+
+    $php_array = json_decode($content, true);
+
+
+    $index = 1;
+    foreach ($php_array['team'] as $person) {
+
+        ?>
+            <div class="col-lg-3 col-sm-6">
+                    <div class="team-box mt-4 position-relative overflow-hidden rounded text-center shadow">
+                        <div class="p-4"><?php
+                        echo '<div class="p-4">';
+                        echo '<h5 class="font-size-19 mb-1">' . '<a href="detail.php">' . $person['name'] . '</a>' . '</h5>';
+                        echo '</div>';
+                        ?></div>
+                    </div>
+                </div>
+            <?php
+        $index++;
+    }
+	echo '<a href="create.php"> <input type="submit"/></a>';
+	
+?>
